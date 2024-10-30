c98fde45ba086f204e31b7598c66c394a3c91efa
croatialu <wuli.croatia@foxmail.com>
2024-10-30 16:23:07 +0800

    feat: version stamp

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# pkg-placeholder
+# vite-plugin-version-stamp
 
 [![npm version][npm-version-src]][npm-version-href]
 [![npm downloads][npm-downloads-src]][npm-downloads-href]
@@ -6,32 +6,19 @@
 [![JSDocs][jsdocs-src]][jsdocs-href]
 [![License][license-src]][license-href]
 
-_description_
-
-> **Note**:
-> Replace `pkg-placeholder`, `_description_` and `antfu` globally to use this template.
-
-## Sponsors
-
-<p align="center">
-  <a href="https://cdn.jsdelivr.net/gh/antfu/static/sponsors.svg">
-    <img src='https://cdn.jsdelivr.net/gh/antfu/static/sponsors.svg'/>
-  </a>
-</p>
-
 ## License
 
-[MIT](./LICENSE) License © 2024-PRESENT [Anthony Fu](https://github.com/antfu)
+[MIT](./LICENSE) License © 2024-PRESENT [Croatia Lu](https://github.com/croatialu)
 
 <!-- Badges -->
 
-[npm-version-src]: https://img.shields.io/npm/v/pkg-placeholder?style=flat&colorA=080f12&colorB=1fa669
-[npm-version-href]: https://npmjs.com/package/pkg-placeholder
-[npm-downloads-src]: https://img.shields.io/npm/dm/pkg-placeholder?style=flat&colorA=080f12&colorB=1fa669
-[npm-downloads-href]: https://npmjs.com/package/pkg-placeholder
-[bundle-src]: https://img.shields.io/bundlephobia/minzip/pkg-placeholder?style=flat&colorA=080f12&colorB=1fa669&label=minzip
-[bundle-href]: https://bundlephobia.com/result?p=pkg-placeholder
-[license-src]: https://img.shields.io/github/license/antfu/pkg-placeholder.svg?style=flat&colorA=080f12&colorB=1fa669
-[license-href]: https://github.com/antfu/pkg-placeholder/blob/main/LICENSE
+[npm-version-src]: https://img.shields.io/npm/v/vite-plugin-version-stamp?style=flat&colorA=080f12&colorB=1fa669
+[npm-version-href]: https://npmjs.com/package/vite-plugin-version-stamp
+[npm-downloads-src]: https://img.shields.io/npm/dm/vite-plugin-version-stamp?style=flat&colorA=080f12&colorB=1fa669
+[npm-downloads-href]: https://npmjs.com/package/vite-plugin-version-stamp
+[bundle-src]: https://img.shields.io/bundlephobia/minzip/vite-plugin-version-stamp?style=flat&colorA=080f12&colorB=1fa669&label=minzip
+[bundle-href]: https://bundlephobia.com/result?p=vite-plugin-version-stamp
+[license-src]: https://img.shields.io/github/license/croatialu/vite-plugin-version-stamp.svg?style=flat&colorA=080f12&colorB=1fa669
+[license-href]: https://github.com/croatialu/vite-plugin-version-stamp/blob/main/LICENSE
 [jsdocs-src]: https://img.shields.io/badge/jsdocs-reference-080f12?style=flat&colorA=080f12&colorB=1fa669
-[jsdocs-href]: https://www.jsdocs.io/package/pkg-placeholder
+[jsdocs-href]: https://www.jsdocs.io/package/vite-plugin-version-stamp
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,137 @@
-export const one = 1
-export const two = 2
+import childProcess from 'node:child_process'
+import { existsSync, readFileSync, writeFileSync } from 'node:fs'
+import { resolve } from 'node:path'
+import process from 'node:process'
+import type { IndexHtmlTransformResult, Plugin } from 'vite'
+
+interface RewriteOptions {
+  name: string
+  version: string
+  shortHash: string
+  fullHash: string
+}
+
+function execCommand(command: string): Promise<string> {
+  const { exec } = childProcess
+
+  return new Promise((resolve, reject) => {
+    exec(command, (error, stdout) => {
+      if (error) {
+        reject(error)
+      }
+      else {
+        const output = stdout.toString()?.replace('\n', '')
+        resolve(output)
+      }
+    })
+  })
+}
+
+function readPackageInfo(): { name: string, version: string } {
+  const packageJSONPath = resolve(process.cwd(), 'package.json')
+
+  if (!existsSync(packageJSONPath)) {
+    throw new Error(`package.json not found at ${packageJSONPath}`)
+  }
+
+  const packageJSON = JSON.parse(readFileSync(packageJSONPath, 'utf-8')) as { name: string, version: string }
+
+  return packageJSON
+}
+
+async function getRewriteOptions(): Promise<RewriteOptions> {
+  const { name, version } = readPackageInfo()
+  const { shortHash, fullHash } = await readGitInfo()
+
+  return {
+    name,
+    version,
+    shortHash,
+    fullHash,
+  }
+}
+
+async function readGitInfo(): Promise<{ shortHash: string, fullHash: string }> {
+  const shortHash = await execCommand('git rev-parse --short HEAD')
+  const fullHash = await execCommand('git rev-parse HEAD')
+
+  return {
+    shortHash,
+    fullHash,
+  }
+}
+
+interface Options {
+  /**
+   * The global name of the version stamp.
+   * @default '__VERSION__'
+   */
+  globalName?: string
+  /**
+   * The rewrite function to transform the version stamp.
+   * @default { RewriteOptions }
+   */
+  version?: ((options: RewriteOptions) => unknown) | unknown
+
+  /**
+   * The output filename.
+   * @default 'version.json'
+   */
+  filename?: string | false
+}
+
+export default (options: Options = {}): Plugin => {
+  const { globalName = '__VERSION__', version, filename = 'version.json' } = options
+  let outputDir: string | undefined
+  let result: unknown
+
+  const getVersionsResult = async (): Promise<unknown> => {
+    if (result)
+      return result
+
+    const rewriteOptions = await getRewriteOptions()
+
+    result = typeof version === 'function' ? version(rewriteOptions) : rewriteOptions
+
+    return result
+  }
+
+  return {
+    name: 'vite-plugin-version-stamp',
+    async config() {
+      const versionResult = await getVersionsResult()
+
+      return {
+        define: {
+          [globalName]: versionResult,
+        },
+      }
+    },
+
+    async transformIndexHtml() {
+      const versionResult = await getVersionsResult()
+
+      const els: IndexHtmlTransformResult = []
+
+      els.push({
+        tag: 'script',
+        injectTo: 'body',
+        children: `${globalName} = ${JSON.stringify(versionResult)}`,
+      })
+
+      return els
+    },
+    configResolved(config) {
+      outputDir = config.build.outDir
+    },
+    async closeBundle() {
+      if (!outputDir || filename === false)
+        return
+
+      const targetFilepath = resolve(outputDir, filename)
+      const versionResult = await getVersionsResult()
+
+      writeFileSync(targetFilepath, JSON.stringify(versionResult, null, 2))
+    },
+  }
+}
